6515d5bc5445887cd55288b7d251caea81f0a7a6
Jakub Hodoši <166517953+jakub-demi@users.noreply.github.com>
2024-04-12 14:33:53 +0200

    add UserController; update providers; update user api route (auth)

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return response()->json($request->user());
-    });
+    Route::get('/user', [UserController::class, 'user']);
 });
