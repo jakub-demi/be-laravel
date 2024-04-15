8f767cc7085ac6ea7f2ddbed433e0847c974b941
Jakub Hodoši <166517953+jakub-demi@users.noreply.github.com>
2024-04-15 12:00:24 +0200

    add Auth provider for SPA password reset (auth)

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+// use Illuminate\Support\Facades\Auth;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        //
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        ResetPassword::createUrlUsing(function (mixed $notifiable, string $token) {
+            $spa_url = env('SPA_URL');
+            $email = $notifiable->getEmailForPasswordReset();
+
+            return "$spa_url/login/?pwdres=1&token=$token&email=$email"; //todo:dev adjust by target URL on FE
+        });
+    }
+}
