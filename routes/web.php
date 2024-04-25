498eb980db746c424a3c4bc2185415efb4d1e822
Jakub Hodoši <166517953+jakub-demi@users.noreply.github.com>
2024-04-25 14:51:41 +0200

    remove testing route (order-items)

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,3 @@ Route::get('/', function () {
 Route::get("/login", function() {
     return redirect()->away(env("SPA_URL") . "/login");
 })->name("login");
-
-
-//todo:dev remove
-Route::get("/t", function () {
-    dd(App\Models\Order::find(5)->order_items()->get());
-});
-
-Route::get("/t2/{orderId}", [App\Http\Controllers\OrderItemController::class, "index"]);
